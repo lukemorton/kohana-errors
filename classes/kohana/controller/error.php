bdc66dedffd1a251c7ca70d0b074fcf1541d5131
Luke Morton <lukemorton.designs@gmail.com>
2011-01-11 22:29:28 +0000

    EOL fix

--- classes/kohana/controller/error.php
+++ classes/kohana/controller/error.php
@@ -1,1 +1,26 @@
-<?php defined('SYSPATH') or die('No direct script access.');class Kohana_Controller_Error extends Controller {		public function before()	{		// External requests default to 404		if ($this->request->is_initial())		{			$this->request->action(404);		}				return parent::before();	}		public function action_404()	{		$this->response->status(404);	}		public function action_500()	{		$this->response->status(500);	}	}
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Kohana_Controller_Error extends Controller {
+	
+	public function before()
+	{
+		// External requests default to 404
+		if ($this->request->is_initial())
+		{
+			$this->request->action(404);
+		}
+		
+		return parent::before();
+	}
+	
+	public function action_404()
+	{
+		$this->response->status(404);
+	}
+	
+	public function action_500()
+	{
+		$this->response->status(500);
+	}
+	
+}
